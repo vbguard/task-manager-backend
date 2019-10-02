d26b4b284db139582251e9a149b7fa4d9be65a25
Liubov Murarova <murarova.lubov@gmail.com>
2019-10-02 18:47:38 +0300

    bearer delete from newUser

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -11,37 +11,49 @@ const getUser = (req, res) => {
 	if (!nickname) {
 		console.log('1');
 
-		res.status(422).json({
-			success: false,
-			message: 'Field nickname empty'
-		}).end();
-  } 
-  
-  if (!password) {
+		res
+			.status(422)
+			.json({
+				success: false,
+				message: 'Field nickname empty'
+			})
+			.end();
+	}
+
+	if (!password) {
 		console.log('2');
 
-		res.status(422).json({
-      success: false,
-			message: 'Field password empty'
-		}).end();
-  } 
-  
-  if (4 < nickname.length > 16) {
+		res
+			.status(422)
+			.json({
+				success: false,
+				message: 'Field password empty'
+			})
+			.end();
+	}
+
+	if (4 < nickname.length > 16) {
 		console.log('3');
-		res.status(400).json({
-      success: false,
-      length: nickname.length,
-			message: 'Nickname length short then 5 or more then 15'
-		}).end();
-  }
-  
-  if (4 < password.length > 13) {
+		res
+			.status(400)
+			.json({
+				success: false,
+				length: nickname.length,
+				message: 'Nickname length short then 5 or more then 15'
+			})
+			.end();
+	}
+
+	if (4 < password.length > 13) {
 		console.log('4');
-		res.status(400).json({
-      success: false,
-      length: password.length,
-			message: 'Password length short then 5 or more then 12'
-		}).end();
+		res
+			.status(400)
+			.json({
+				success: false,
+				length: password.length,
+				message: 'Password length short then 5 or more then 12'
+			})
+			.end();
 	} else {
 		console.log('5');
 
@@ -54,45 +66,63 @@ const getUser = (req, res) => {
 						.save()
 						.then(user => {
 							const token = jwt.sign({userId: user._id}, jwtSecretKey);
-							res.status(200).json({
-								nickname: user.nickname,
-								token: `Bearer ${token}`
-							}).end();
+							res
+								.status(200)
+								.json({
+									nickname: user.nickname,
+									token
+								})
+								.end();
 						})
 						.catch(err => {
-							res.status(500).json({
-								success: false,
-								message: err.message,
-								error: err
-							}).end();
+							res
+								.status(500)
+								.json({
+									success: false,
+									message: err.message,
+									error: err
+								})
+								.end();
 						});
 				} else {
 					user.comparePassword(password, (err, isMatch) => {
 						if (err) {
-							res.status(500).json({
-								success: false,
-								message: err.message,
-								error: err
-							}).end();
+							res
+								.status(500)
+								.json({
+									success: false,
+									message: err.message,
+									error: err
+								})
+								.end();
 						}
 
 						if (isMatch) {
 							const token = jwt.sign({userId: user._id}, jwtSecretKey);
-							res.status(200).json({
-								nickname: user.nickname,
-								token
-							}).end();
+							res
+								.status(200)
+								.json({
+									nickname: user.nickname,
+									token
+								})
+								.end();
 						} else {
-							res.status(400).json({
-								success: false,
-								message: 'Invalid Password'
-							}).end();
+							res
+								.status(400)
+								.json({
+									success: false,
+									message: 'Invalid Password'
+								})
+								.end();
 						}
 					});
 				}
 			})
 			.catch(err => {
-				res.status(400).json({message: 'Invalid Password/Username'}).end();
+				res
+					.status(400)
+					.json({message: 'Invalid Password/Username'})
+					.end();
 			});
 	}
 };
